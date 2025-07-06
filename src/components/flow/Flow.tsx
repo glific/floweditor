e27d5227f91598473661ecd43550c44b224433a7
Akansha Sakhre <asakhre2002@gmail.com>
2025-07-06 10:18:54 +0530

    refactor: Update temba-components dependency and improve mobile handling in Flow component

--- src/components/flow/Flow.tsx
+++ src/components/flow/Flow.tsx
@@ -146,6 +146,11 @@ export class Flow extends React.PureComponent<FlowStoreProps, {}> {
     return (this.ghost = ref);
   }
 
+  public isMobile() {
+    const win = window as any;
+    return win.isMobile && win.isMobile();
+  }
+
   public componentDidMount(): void {
     this.Plumber.bind('connection', (event: ConnectionEvent) =>
       this.props.updateConnection(event.sourceId, event.targetId)
@@ -210,7 +215,10 @@ export class Flow extends React.PureComponent<FlowStoreProps, {}> {
 
       // Save our position for later
       const { left, top } = (this.ghost &&
-        snapToGrid(this.ghost.ele.offsetLeft, this.ghost.ele.offsetTop)) || { left: 0, top: 0 };
+        snapToGrid(this.ghost.ele.offsetLeft, this.ghost.ele.offsetTop)) || {
+        left: 0,
+        top: 0
+      };
 
       this.props.ghostNode.ui.position = { left, top };
 
@@ -409,7 +417,7 @@ export class Flow extends React.PureComponent<FlowStoreProps, {}> {
         {this.getNodeEditor()}
 
         <Canvas
-          mutable={this.context.config.mutable}
+          mutable={!this.isMobile() && this.context.config.mutable}
           draggingNew={!!this.props.ghostNode && !this.props.nodeEditorSettings}
           newDragElement={this.getDragNode()}
           onDragging={this.handleDragging}
