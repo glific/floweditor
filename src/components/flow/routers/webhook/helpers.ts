4929a9abf7158c59edea16fb9bff5e24aab884db
AmishaBisht <amishabisht33@gmail.com>
2025-10-30 13:40:31 +0530

    fix: update the snapshot state structure

--- src/components/flow/routers/webhook/helpers.ts
+++ src/components/flow/routers/webhook/helpers.ts
@@ -64,6 +64,7 @@ export const nodeToState = (settings: NodeEditorSettings): WebhookRouterFormStat
     url: { value: '' },
     body: { value: getDefaultBody(Methods.GET) },
     webhookFunction: { value: null },
+    webhookOptions: [],
     valid: false
   };
 
