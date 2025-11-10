616f132da25dff65bcfcb167a8368ef379fec1d4
Akansha Sakhre <asakhre2002@gmail.com>
2025-11-10 23:37:59 +0530

    Add webhook function handling and options to WebhookRouterForm

--- src/components/flow/routers/webhook/helpers.ts
+++ src/components/flow/routers/webhook/helpers.ts
@@ -63,6 +63,8 @@ export const nodeToState = (settings: NodeEditorSettings): WebhookRouterFormStat
     method: { value: GET_METHOD },
     url: { value: '' },
     body: { value: getDefaultBody(Methods.GET) },
+    webhookFunction: { value: null },
+    webhookOptions: [],
     valid: false
   };
 
